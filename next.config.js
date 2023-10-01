89da990b8574afc94cf75aa931e4cec97b42e1a5
nakimura <nakimura@nakimuranoMacBook-Air.local>
2023-10-02 00:15:56 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+    
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境では React Testing Library で使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+    
+    return compilerConfig
+  })(),
+}
+
+module.exports = nextConfig
